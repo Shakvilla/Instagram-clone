b5f8d6caf9bcd913e01018ef755e22a55576bc51
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-03 20:44:51 +0000

    post feature created

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import colors from './src/theme/colors';
-import font from './src/theme/fonts';
+import {View, StyleSheet, Image} from 'react-native';
+
 const App = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: colors.primary, fontSize: font.size.lg}}>
-        Hello world
-      </Text>
+    <View style={styles.post}>
+      {/* Header  */}
+      {/* Content */}
+      <Image
+        source={{
+          uri: 'https://images.unsplash.com/photo-1582876533492-51fd2f162272?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
+        }}
+        style={styles.image}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  post: {},
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
 
 export default App;
