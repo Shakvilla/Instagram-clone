102c942196f0c3209772497d2be57c27234bcee8
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-03 17:12:09 +0000

    added font style to theme object

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import colors from './src/theme/colors';
-
+import font from './src/theme/fonts';
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{color: colors.primary}}>Hello world</Text>
+      <Text style={{color: colors.primary, fontSize: font.size.lg}}>
+        Hello world
+      </Text>
     </View>
   );
 };
