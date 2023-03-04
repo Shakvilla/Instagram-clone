8eb87e1944c888f952c3c67495d5d8d1fa37cdd9
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-04 00:05:00 +0000

    adding liked comments

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
-
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import colors from './src/theme/colors';
+import fonts from './src/theme/fonts';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feather from 'react-native-vector-icons/Feather';
 const App = () => {
+  const [isLiked, setIsLiked] = useState('');
   return (
     <View style={styles.post}>
       {/* Header  */}
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1582876533492-51fd2f162272?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>monica</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
       {/* Content */}
       <Image
         source={{
@@ -12,15 +32,84 @@ const App = () => {
         }}
         style={styles.image}
       />
+
+      {/* Footer View */}
+      <View style={styles.footer}>
+        {/* icon container */}
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name={isLiked ? 'heart' : 'hearto'}
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+
+        <Text>
+          Liked by <Text style={styles.boldText}>shakvilla</Text> and{' '}
+          <Text style={styles.boldText}>237 others</Text>
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   post: {},
+
   image: {
     width: '100%',
     aspectRatio: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  userAvatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontWeight: fonts.weight.bold,
+    color: colors.black,
+  },
+  threeDots: {
+    marginLeft: 'auto',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+
+  icon: {
+    marginHorizontal: 5,
+  },
+  footer: {
+    padding: 10,
+  },
+  boldText: {
+    fontWeight: fonts.weight.bold,
   },
 });
 
