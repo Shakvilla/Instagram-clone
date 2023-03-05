9e18400ad877ed6aff9fd1ba271c58288e36eb90
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-05 23:14:33 +0000

    added carousel component

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,5 @@
-import {Image, useWindowDimensions} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {View, Image, useWindowDimensions} from 'react-native';
 import React from 'react';
 import {FlashList} from '@shopify/flash-list';
 
@@ -9,15 +10,50 @@ interface ICarousel {
 const Carousel = ({images}: ICarousel) => {
   const {width} = useWindowDimensions();
   return (
-    <FlashList
-      data={images}
-      horizontal
-      pagingEnabled
-      estimatedItemSize={359}
-      renderItem={({item}) => (
-        <Image source={{uri: item}} style={{width, aspectRatio: 1}} />
-      )}
-    />
+    <>
+      <FlashList
+        data={images}
+        horizontal
+        pagingEnabled
+        estimatedItemSize={359}
+        renderItem={({item}) => (
+          <Image source={{uri: item}} style={{width, aspectRatio: 1}} />
+        )}
+      />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+        }}>
+        <View
+          style={{
+            width: 10,
+            aspectRatio: 1,
+            borderRadius: 5,
+            backgroundColor: 'red',
+          }}
+        />
+        <View
+          style={{
+            width: 10,
+            aspectRatio: 1,
+            borderRadius: 5,
+            backgroundColor: 'red',
+          }}
+        />
+        <View
+          style={{
+            width: 10,
+            aspectRatio: 1,
+            borderRadius: 5,
+            backgroundColor: 'red',
+          }}
+        />
+      </View>
+    </>
   );
 };
 
