b354cbb72ae5470452adbc2a4b77922e059e4b08
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-08 15:07:09 +0000

    added video feature

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {ViewabilityConfig, FlatList, ViewToken} from 'react-native';
 import NewsFeed from '../../components/NewsFeed';
 import posts from '../../assets/data/post.json';
-import {FlashList} from '@shopify/flash-list';
+// import {FlashList} from '@shopify/flash-list';
 
 const HomeScreen = () => {
+  const [activePostId, setActivePostId] = useState<string | null>(null);
+
+  const viewabilityConfig: ViewabilityConfig = {
+    itemVisiblePercentThreshold: 51,
+  };
+
+  const onViewableItemsChanged = useRef(
+    ({viewableItems}: {viewableItems: Array<ViewToken>}) => {
+      if (viewableItems.length > 0) {
+        setActivePostId(viewableItems[0].item.id);
+      }
+    },
+  );
   return (
-    <FlashList
+    <FlatList
       data={posts}
-      renderItem={({item}) => <NewsFeed post={item} />}
-      estimatedItemSize={20}
+      renderItem={({item}) => (
+        <NewsFeed post={item} isVisible={activePostId === item.id} />
+      )}
+      // estimatedItemSize={20}
       keyExtractor={item => {
         return item.id;
       }}
       showsVerticalScrollIndicator={false}
+      viewabilityConfig={viewabilityConfig}
+      onViewableItemsChanged={onViewableItemsChanged.current}
     />
   );
 };
