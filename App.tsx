b6ecdc7f45e4e018339d19c1b71f45118892dfcd
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-10 11:28:48 +0000

    refactoring profile page

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 // import CommentsScreen from './src/screens/CommentsScreen/CommentsScreen';
-import HomeScreen from './src/screens/HomeScreen';
-// import ProfileScreen from './src/screens/ProfileScreen';
+// import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 
 const App = () => {
   return (
     <View style={styles.app}>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
       {/* <CommentsScreen /> */}
-      {/* <ProfileScreen /> */}
+      <ProfileScreen />
     </View>
   );
 };
--- src/screens/ProfileScreen/ProfileScreen.tsx
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,45 +1,9 @@
-import {View, Image, Text, FlatList, StyleSheet} from 'react-native';
+import {View, Image} from 'react-native';
 import React, {memo} from 'react';
 import styles from './styles';
 import user from '../../assets/data/user.json';
-import Button from '../../components/Button';
 import {FlashList} from '@shopify/flash-list';
-const ProfileHeader = () => {
-  return (
-    <View style={styles.root}>
-      <View style={styles.headerRow}>
-        {/* Profile image */}
-        <Image source={{uri: user.image}} style={styles.avatar} />
-        {/* Posts, followers, following numbers */}
-        <View style={styles.numberContainer}>
-          <Text style={styles.numberText}>243</Text>
-          <Text>Posts</Text>
-        </View>
-        <View style={styles.numberContainer}>
-          <Text style={styles.numberText}>9219</Text>
-          <Text>Followers</Text>
-        </View>
-        <View style={styles.numberContainer}>
-          <Text style={styles.numberText}>101</Text>
-          <Text>Following</Text>
-        </View>
-      </View>
-      <Text style={styles.name}>{user.name}</Text>
-      <Text>{user.bio}</Text>
-      <View style={styles.buttonsRoot}>
-        <Button
-          text="Edit Profile"
-          onPress={() => console.warn('Editing Profile')}
-        />
-        <Button
-          text="Another Button"
-          onPress={() => console.warn('Editing Profile')}
-        />
-      </View>
-    </View>
-  );
-};
-
+import ProfileHeader from './ProfileHeader';
 const ProfileScreen = () => {
   return (
     <View style={styles.gallery}>
