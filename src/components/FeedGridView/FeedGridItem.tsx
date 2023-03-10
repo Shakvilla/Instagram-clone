7133c0e2c1db654a24f403e3590988f232d77be2
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-10 16:31:43 +0000

    added collections of images

--- src/components/FeedGridView/FeedGridItem.tsx
+++ src/components/FeedGridView/FeedGridItem.tsx
@@ -2,6 +2,8 @@ import {View, Image} from 'react-native';
 import React from 'react';
 import {IPost} from '../../types/models';
 import styles from '../../screens/ProfileScreen/styles';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import colors from '../../theme/colors';
 const FeedGridItem = ({post}: {post: IPost}) => {
   return (
     <View style={styles.galleryImage}>
@@ -9,6 +11,14 @@ const FeedGridItem = ({post}: {post: IPost}) => {
         source={{uri: post.image || post.images[0]}}
         style={styles.gallery}
       />
+      {post.images && (
+        <MaterialIcons
+          name="collections"
+          size={16}
+          color={colors.white}
+          style={{position: 'absolute', top: 5, right: 5}}
+        />
+      )}
     </View>
   );
 };
