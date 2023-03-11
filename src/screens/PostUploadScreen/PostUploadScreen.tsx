ae9160eced7435a01c60bc2440966a244b2daaef
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-11 21:57:26 +0000

    added expo install modules. This allows me to use any expo app

---
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const PostUploadScreen = () => {
+  return (
+    <View>
+      <Text>PostUploadScreen</Text>
+    </View>
+  );
+};
+
+export default PostUploadScreen;
