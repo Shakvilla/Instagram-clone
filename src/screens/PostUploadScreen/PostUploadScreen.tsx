c625f1feb5de57a71389dbc2fa2c027b9157f0a1
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-11 23:47:51 +0000

    using camera in the project now

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,7 +1,32 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Camera} from 'expo-camera';
 
 const PostUploadScreen = () => {
+  const [hasCameraAccessPermission, setHasCameraAccessPermission] = useState<
+    boolean | null
+  >(null);
+
+  useEffect(() => {
+    const getPermission = async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const microphonePermission = Camera.requestMicrophonePermissionsAsync();
+
+      setHasCameraAccessPermission(
+        cameraPermission.status === 'granted' &&
+          microphonePermission.status === 'granted',
+      );
+    };
+    getPermission();
+  }, []);
+
+  if (hasCameraAccessPermission === null) {
+    return <Text>Loading</Text>;
+  }
+
+  if (hasCameraAccessPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View>
       <Text>PostUploadScreen</Text>
