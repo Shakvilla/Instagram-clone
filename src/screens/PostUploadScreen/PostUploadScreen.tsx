d6e3b34b6ce73b395a41462437cbec912be63376
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-12 14:24:58 +0000

    implementing camera

--- src/screens/PostUploadScreen/PostUploadScreen.tsx
+++ src/screens/PostUploadScreen/PostUploadScreen.tsx
@@ -1,15 +1,28 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {Camera, CameraType} from 'expo-camera';
+import {Camera, CameraType, FlashMode} from 'expo-camera';
 import colors from '../../theme/colors';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+const flashModes = [
+  FlashMode.off,
+  FlashMode.on,
+  FlashMode.auto,
+  FlashMode.torch,
+];
+
+const flashModeToIcon = {
+  [FlashMode.off]: 'flash-off',
+  [FlashMode.on]: 'flash-on',
+  [FlashMode.auto]: 'flash-auto',
+  [FlashMode.torch]: 'highlight',
+};
 const PostUploadScreen = () => {
   const [hasCameraAccessPermission, setHasCameraAccessPermission] = useState<
     boolean | null
   >(null);
   const [type, setType] = useState(CameraType.back);
-
+  const [flash, setFlash] = useState(FlashMode.off);
   useEffect(() => {
     const getPermission = async () => {
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
@@ -32,28 +45,42 @@ const PostUploadScreen = () => {
     return <Text>No access to camera</Text>;
   }
 
-  function toggleCameraType() {
+  const toggleCameraType = () => {
     setType(current =>
       current === CameraType.back ? CameraType.front : CameraType.back,
     );
-  }
+  };
+
+  const toggleFlashMode = () => {
+    const currIndex = flashModes.indexOf(flash);
+    const nextIndex = currIndex === flashModes.length - 1 ? 0 : currIndex + 1;
+    setFlash(flashModes[nextIndex]);
+  };
+  console.warn(flash);
   return (
     <View style={styles.page}>
-      <Camera style={styles.camera} type={type} />
+      <Camera style={styles.camera} type={type} ratio="4:3" flashMode={flash} />
       <View style={[styles.buttonsContainer, {top: 25}]}>
         <MaterialIcons name="close" size={30} color={colors.white} />
-        <MaterialIcons name="flash-off" size={30} color={colors.white} />
+        <Pressable onPress={toggleFlashMode}>
+          <MaterialIcons
+            name={flashModeToIcon[flash]}
+            size={30}
+            color={colors.white}
+          />
+        </Pressable>
         <MaterialIcons name="settings" size={30} color={colors.white} />
       </View>
       <View style={[styles.buttonsContainer, {bottom: 25}]}>
         <MaterialIcons name="photo-library" size={30} color={colors.white} />
         <View style={styles.circle} />
-        <MaterialIcons
-          name="flip-camera-ios"
-          size={30}
-          color={colors.white}
-          onPress={toggleCameraType}
-        />
+        <Pressable onPress={toggleCameraType}>
+          <MaterialIcons
+            name="flip-camera-ios"
+            size={30}
+            color={colors.white}
+          />
+        </Pressable>
       </View>
     </View>
   );
