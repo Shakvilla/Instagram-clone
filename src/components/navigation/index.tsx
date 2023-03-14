85b358e735f5d7b93a9fc778b1319061adce51a4
Shakvilla <abdulshakuraclement@yahoo.com>
2023-03-14 00:13:15 +0000

    refactoring ui

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from '../../screens/HomeScreen';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ProfileScreen from '../../screens/ProfileScreen';
+import {Image} from 'react-native';
+import logo from '../../assets/images/instagram.png';
+
+const Stack = createNativeStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <HomeScreen />
+      <Stack.Navigator
+        initialRouteName="UserProfile"
+        screenOptions={{header: HeaderTitle, headerTitleAlign: 'center'}}>
+        <Stack.Group>
+          <Stack.Screen name="Feed" component={HomeScreen} />
+          <Stack.Screen
+            name="UserProfile"
+            component={ProfileScreen}
+            options={{title: 'Profile'}}
+          />
+        </Stack.Group>
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+};
+
+const HeaderTitle = () => {
+  return (
+    <Image
+      source={logo}
+      resizeMode="contain"
+      style={{width: 150, height: 40}}
+    />
   );
 };
 
