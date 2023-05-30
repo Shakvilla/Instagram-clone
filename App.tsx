00b1c1ff8853e05559cb31d4c2a0dbd514dec9d5
Shakvilla <abdulshakuraclement@yahoo.com>
2023-05-30 00:29:53 +0000

    added aws amplify backend

--- App.tsx
+++ App.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import Navigation from './src/components/navigation';
+import {Amplify} from 'aws-amplify';
+import config from './src/aws-exports';
 
+Amplify.configure(config);
 const App = () => {
   return (
     <SafeAreaProvider>
