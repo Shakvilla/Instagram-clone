dbe21d952e0a11d4adbe323a878feed4bd9d4e83
Shakvilla <abdulshakuraclement@yahoo.com>
2023-06-01 09:01:17 +0000

    Refactoring ui

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+// import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import Navigation from './src/components/navigation';
 import {Amplify} from 'aws-amplify';
 import config from './src/aws-exports';
+import {withAuthenticator} from 'aws-amplify-react-native';
 
 Amplify.configure(config);
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <Navigation />
-    </SafeAreaProvider>
+    // <SafeAreaProvider>
+    <Navigation />
+    // </SafeAreaProvider>
   );
 };
 
-export default App;
+export default withAuthenticator(App);
