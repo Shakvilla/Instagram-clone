b4c42eb84d7f3cfb7d1d68799753e3ed91767062
Shakvilla <abdulshakuraclement@yahoo.com>
2023-06-02 00:54:13 +0000

    implemented amplify ui

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-// import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import Navigation from './src/components/navigation';
 import {Amplify} from 'aws-amplify';
 import config from './src/aws-exports';
-import {withAuthenticator} from 'aws-amplify-react-native';
+import {withAuthenticator} from '@aws-amplify/ui-react-native';
 
 Amplify.configure(config);
 const App = () => {
   return (
-    // <SafeAreaProvider>
-    <Navigation />
-    // </SafeAreaProvider>
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
   );
 };
 
-export default withAuthenticator(App);
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+      placeholder: 'Name',
+    },
+  ],
+};
+export default withAuthenticator(App, {signUpConfig});
